83470925218868d6ab121d1b9429199bb57bccff
MartinMurithi <wachiramartin442@gmail.com>
2023-09-01 00:19:53 +0300

    ..

--- server/config/multer.js
+++ server/config/multer.js
@@ -12,7 +12,7 @@ const storage = multer.diskStorage({
 });
 
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png" || file.mimetype === "image/webp") {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png" || file.mimetype === "image/webp", file.mimetype === "image/avif") {
     cb(null, true);
   } else {
     cb(
